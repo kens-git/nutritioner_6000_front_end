1200aa16f9eae0ea0e2ff4ee620be6d522ae2272
Ken Montgomery <ID+username@users.noreply.github.com>
2022-10-10 20:27:24 -0400

    Implemented InputIntakeForm's submission.

--- src/components/forms/InputIntakeForm.tsx
+++ src/components/forms/InputIntakeForm.tsx
@@ -1,21 +1,53 @@
+import { useContext, useRef, useState } from 'react';
 import ConsumableDataContext
   from "../../store/ConsumableDataContext";
 import Select, { extractConsumableItem } from "../ui/Select";
 import { button_classes, form_classes, input_classes }
   from "../tailwind_classes";
+import IntakeDataContext from '../../store/IntakeDataContext';
+import Consumable from '../../types/Consumable';
 
 const InputIntakeForm: React.FC<{}> = (props) => {
+  const [consumable, setConsumable] = useState<Consumable>();
+  const [date, setDate] = useState<string>(new Date().toLocaleDateString('en-CA'));
+  const [time, setTime] = useState<string>(
+    new Date().toLocaleTimeString('en-CA', { hour12: false }));
+  const servingSizeRef = useRef<HTMLInputElement>(null);
+  const consumableCtx = useContext(ConsumableDataContext);
+  const intakeCtx = useContext(IntakeDataContext);
+
+  const onDateChanged = (event: any) => {
+    setDate(event.target.value);
+  };
+
+  const onTimeChanged = (event: any) => {
+    setTime(event.target.value);
+  };
+
   const onSubmit = (event: any) => {
     event.preventDefault();
+    intakeCtx.add({
+      id: -1,
+      timestamp: new Date(date + 'T' + time),
+      consumable: consumable!,
+      serving_size: +servingSizeRef.current!.value
+    });
   };
 
   return (
+    // TODO: if no consumables exist, hide this form
+    // TODO: check other cases where values may be null, and hide corresponding components
     <form onSubmit={onSubmit} className={form_classes}>
       <Select id='input-intake-form-name' name='name'
         dataContext={ConsumableDataContext}
+        onChange={setConsumable}
         extractItem={extractConsumableItem} />
-      <input className={input_classes} type='datetime-local' defaultValue='2022-01-01 9:30 PM' />
-      <input className={input_classes} type='number' placeholder='Serving Size' />
+      <input className={input_classes} type='date'
+        onChange={onDateChanged} defaultValue={date} required />
+      <input className={input_classes} type='time'
+        onChange={onTimeChanged} defaultValue={time} step={1} required />
+      <input ref={servingSizeRef} className={input_classes} type='number'
+        placeholder='Serving Size' min={0} required />
       <button className={button_classes}
         type='submit'>Submit</button>
     </form>

8e61d4112536f16f44fcc92b07f426afcab06975
Ken Montgomery <ID+username@users.noreply.github.com>
2022-10-10 20:11:23 -0400

    Moved SelectItem into Select file, fixed extract methods to return strings.

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -3,38 +3,44 @@ import Consumable from '../../types/Consumable';
 import ConsumableCategory from '../../types/ConsumableCategory';
 import Name from '../../types/Name';
 import Nutrient from '../../types/Nutrient';
-import SelectItem from '../../types/SelectItem';
 import Unit from '../../types/Unit';
 
-export const extractConsumableItem = (consumable: Consumable) => {
+export interface SelectItem {
+  id: string;
+  label: string;
+}
+
+export const extractConsumableItem =
+    (consumable: Consumable): SelectItem => {
   return {
-    id: consumable.id,
+    id: consumable.id.toString(),
     label: consumable.name
   }
 }
 
-export const extractConsumableCategoryItem = (category: ConsumableCategory) => {
+export const extractConsumableCategoryItem =
+    (category: ConsumableCategory): SelectItem => {
   return {
-    id: category.id,
+    id: category.id.toString(),
     label: category.name.name
   }
 }
 
-export const extractNameItem = (name: Name) => {
+export const extractNameItem = (name: Name): SelectItem => {
   return {
     id: name.id.toString(),
     label: name.name
   }
 };
 
-export const extractNutrientItem = (nutrient: Nutrient) => {
+export const extractNutrientItem = (nutrient: Nutrient): SelectItem => {
   return {
     id: nutrient.id.toString(),
     label: nutrient.name.name
   }
 }
 
-export const extractUnitItem = (unit: Unit) => {
+export const extractUnitItem = (unit: Unit): SelectItem => {
   return {
     id: unit.id.toString(),
     label: unit.name.name
--- src/types/SelectItem.ts
+++
@@ -1,6 +0,0 @@
-interface SelectItem {
-  id: string;
-  label: string;
-}
-
-export default SelectItem;

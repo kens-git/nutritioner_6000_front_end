09932272e49430b1fc688c7765eba725d6cfe049
Ken Montgomery <ID+username@users.noreply.github.com>
2022-10-20 19:52:37 -0400

    Documented LoginResponse.

--- src/types/response/LoginResponse.ts
+++ src/types/response/LoginResponse.ts
@@ -1,5 +1,10 @@
+/** Defines the response of a login request. */
 interface LoginResponse {
+
+  /** The current user's authentication token. */
   token: string;
+
+  /** The current user's id. */
   user_id: number;
 }
 

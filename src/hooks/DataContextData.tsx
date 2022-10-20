bb62e87a3c43095409c556966346208e990a93a8
Ken Montgomery <ID+username@users.noreply.github.com>
2022-10-20 18:40:36 -0400

    Documented custom hooks.

--- src/hooks/DataContextData.tsx
+++ src/hooks/DataContextData.tsx
@@ -3,15 +3,20 @@ import { ContextDataBase, DataContextData }
   from '../store/DataContext';
 import Id from "../types/Id";
 
+/**
+ * A hook to hide the details of a DataContext by providing
+ * a single value that updates automatically.
+ * 
+ * @template T The context's data type.
+ * @template U The context's submission type.
+ * @param context The context to get data from.
+ * @returns A state containing the DataContext's current data.
+ */
 const useDataContextData =
     <T extends ContextDataBase, U>(context: React.Context<
       DataContextData<T, U>>) => {
   const dataCtx = useContext(context);
   const [data, setData] = useState<Map<Id, T>>(new Map<Id, T>());
-  
-  const onLoad = (data: Map<Id, T>) => {
-    setData(data);
-  }
   
   useEffect(() => {
     setData(dataCtx.data);
--- src/hooks/FormattedDataContextData.tsx
+++ src/hooks/FormattedDataContextData.tsx
@@ -4,6 +4,18 @@ import { ContextDataBase, DataContextData }
 import Id from "../types/Id";
 import useDataContextData from './DataContextData';
 
+/**
+ * A hook that wraps the useDataContextData hook and provides a formatted
+ * view of a DataContext's data.
+ * 
+ * @template T The context's data type.
+ * @template U The context's submission type.
+ * @template V The format type.
+ * @param context The DataContext to use.
+ * @param format A Function that takes the context's data and returns
+ *               it as a list of the specified type.
+ * @returns A state containing the formatted data.
+ */
 const useFormattedDataContextData = <T extends ContextDataBase, U, V>(
     context: React.Context<DataContextData<T, U>>,
     format: (data: Map<Id, T>) => V[]) => {

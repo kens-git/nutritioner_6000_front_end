3b79175634ea7cbffdca985af47ad06a2c9b3d53
Ken Montgomery <ID+username@users.noreply.github.com>
2022-10-20 16:46:26 -0400

    Documented PageLayout.

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -2,6 +2,9 @@ import { useContext } from 'react';
 import AuthContext from "../../store/AuthContext";
 import HeaderNavigation from "../navigation/HeaderNavigation";
 
+/**
+ * The page layout for standard/generic pages.
+ */
 const PageLayout: React.FC<{children: React.ReactNode}> = (props) => {
   const authCtx = useContext(AuthContext);
 
